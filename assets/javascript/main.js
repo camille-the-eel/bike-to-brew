e263cdc4f103eb21bc1521e25de1ac345a9dae17
Zkinsk <zoolunk@gmail.com>
2019-01-02 21:04:10 -0500

    working on map points

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -163,92 +163,99 @@ function markerMap(mapCtr, mapInfoArr) {
     var latLong2 = {lat: 37.5407, lng: -77.5360};
     var latLong3 = {lat: 37.5407, lng: -77.3360};
     
-    var map = new google.maps.Map(
+    map = new google.maps.Map(
         document.getElementById("markerMap"), {
-            zoom: 12, 
-            center: mapCtr, 
+            zoom: 8, 
+            center: latLong, 
             styles: [
-                {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-                {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-                {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-                {
-                featureType: 'administrative.locality',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#d59563'}]
+                {elementType: "geometry", 
+                stylers: [{color: "#242f3e"}]
+                },
+                {elementType: "labels.text.stroke", 
+                stylers: [{color: "#242f3e"}]
+                },
+                {elementType: "labels.text.fill", 
+                stylers: [{color: "#746855"}]
                 },
                 {
-                featureType: 'poi',
-                elementType: 'labels.text.fill',
-                stylers: [{visibility: 'off'}]
+                featureType: "administrative.locality",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#d59563"}]
                 },
                 {
-                featureType: 'poi.park',
-                elementType: 'geometry',
-                stylers: [{color: '#263c3f'}]
+                featureType: "poi",
+                elementType: "labels.text.fill",
+                stylers: [{visibility: "off"}]
                 },
                 {
-                featureType: 'poi.park',
-                elementType: 'labels.text.fill',
-                stylers: [{visibility: 'off'}]
+                featureType: "poi.park",
+                elementType: "geometry",
+                stylers: [{color: "#263c3f"}]
                 },
                 {
-                featureType: 'road',
-                elementType: 'geometry',
-                stylers: [{color: '#38414e'}]
+                featureType: "poi.park",
+                elementType: "labels.text.fill",
+                stylers: [{visibility: "off"}]
                 },
                 {
-                featureType: 'road',
-                elementType: 'geometry.stroke',
-                stylers: [{color: '#212a37'}]
+                featureType: "road",
+                elementType: "geometry",
+                stylers: [{color: "#38414e"}]
                 },
                 {
-                featureType: 'road',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#9ca5b3'}]
+                featureType: "road",
+                elementType: "geometry.stroke",
+                stylers: [{color: "#212a37"}]
                 },
                 {
-                featureType: 'road.highway',
-                elementType: 'geometry',
-                stylers: [{color: '#746855'}]
+                featureType: "road",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#9ca5b3"}]
                 },
                 {
-                featureType: 'road.highway',
-                elementType: 'geometry.stroke',
-                stylers: [{color: '#1f2835'}]
+                featureType: "road.highway",
+                elementType: "geometry",
+                stylers: [{color: "#746855"}]
                 },
                 {
-                featureType: 'road.highway',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#f3d19c'}]
+                featureType: "road.highway",
+                elementType: "geometry.stroke",
+                stylers: [{color: "#1f2835"}]
                 },
                 {
-                featureType: 'transit',
-                elementType: 'geometry',
-                stylers: [{visibility: 'off'}]
+                featureType: "road.highway",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#f3d19c"}]
                 },
                 {
-                featureType: 'transit.station',
-                elementType: 'labels.text.fill',
-                stylers: [{visibility: 'off'}]
+                featureType: "transit",
+                elementType: "geometry",
+                stylers: [{visibility: "off"}]
                 },
                 {
-                featureType: 'water',
-                elementType: 'geometry',
-                stylers: [{color: '#17263c'}]
+                featureType: "transit.station",
+                elementType: "labels.text.fill",
+                stylers: [{visibility: "off"}]
                 },
                 {
-                featureType: 'water',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#515c6d'}]
+                featureType: "water",
+                elementType: "geometry",
+                stylers: [{color: "#17263c"}]
                 },
                 {
-                featureType: 'water',
-                elementType: 'labels.text.stroke',
-                stylers: [{color: '#17263c'}]
+                featureType: "water",
+                elementType: "labels.text.fill",
+                stylers: [{color: "#515c6d"}]
+                },
+                {
+                featureType: "water",
+                elementType: "labels.text.stroke",
+                stylers: [{color: "#17263c"}]
             }
-        ]
-    });
-
+        ]   
+    
+    }); 
+    
     for(let i = 0; i < mapInfoArr.length; i++){
         let position = {lat: mapInfoArr[i].lat, lng: mapInfoArr[i].lon}
         console.log(position);
