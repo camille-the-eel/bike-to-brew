1f3e1ddfd1f2a88dc38ef83f1a32d6f9a880372a
Zkinsk <zoolunk@gmail.com>
2019-01-11 00:02:12 -0500

    tweaking popup

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -769,6 +769,7 @@ function infoWindowPopup(marker) {
     // infowindow.open(map, marker);
   }
   infowindow.open(map, marker);
+  $(".popUp :parent").css({"background-color": "red"})
 }
 
 // pans map to map marker when selecting from one of the lists
